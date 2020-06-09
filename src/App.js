afc581de19b5b24d200670cb3d479d4150ec015a
mauriciorosito <mauricio.rosito@bento.ifrs.edu.br>
2020-06-08 21:58:49 -0300

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ useState } from 'react';
 import './App.css';
+import { cpfMask } from './mask'
 
 function App() {
+  const [state, setState] = useState({ documentId: '' });
+   
+  function handlechange(e) {
+    setState({ documentId: cpfMask(e.target.value) });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>CPF:</label>
+        <input 
+          maxLength='14'
+          name='documentId'
+          value={state.documentId}
+          onChange={handlechange}
+        />
     </div>
   );
 }
